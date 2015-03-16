2d22e9e615e5192e2f1ff4761bf5dc2b1347a7a6
173210 <root.3.173210@live.com>
2015-03-17 07:47:18 +0900

    arm9.s: Clear screen

--- arm9.s
+++ arm9.s
@@ -5,5 +5,13 @@
 	.global	_start
 	.type	_start, %function
 _start:
-	b	_start
+	ldr	r1, =0x18600000
+	mov	r2, #0xFFFFFFFF
+.clearScr:
+	mov	r0, #0x18000000
+.loop:
+	str	r2, [r0], #4
+	cmp	r0, r1
+	bcc	.loop
+	b	.clearScr
 	.size	_start, .-_start
