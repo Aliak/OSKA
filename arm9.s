5e3263e8760262b22565adfb380f70dba6d91c42
173210 <root.3.173210@live.com>
2015-03-17 08:09:32 +0900

    Update arm9.s

--- arm9.s
+++ arm9.s
@@ -5,13 +5,13 @@
 	.global	_start
 	.type	_start, %function
 _start:
-	ldr	r1, =0x18600000
-	mov	r2, #0xFFFFFFFF
-.clearScr:
 	mov	r0, #0x18000000
+	add	r1, r0, #0x00600000
+	mov	r2, #0xFFFFFFFF
 .loop:
 	str	r2, [r0], #4
 	cmp	r0, r1
 	bcc	.loop
-	b	.clearScr
+	mov	r0, #0x18000000
+	b	.loop
 	.size	_start, .-_start
